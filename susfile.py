cab5a7d2de74ec6ab1058853b61b7d333ac42bb4
JWM7210 <infinite8391@gmail.com>
2025-02-28 09:30:35 -0500

    Create susfile.py

---
+++ susfile.py
@@ -0,0 +1,4 @@
+susfile = "You've been Hacked...not"
+print("Youre Files Are Encrypted, You Must Pay 300 Hawk Tuwah Coin to Get your Files Back.")
+print("You are Hacked. I shall now clap in self-accomplishment.")
+print(susfile)
